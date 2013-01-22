4e7b392fcd422d44f26b037b5b5b00a6a4ea2f60
Aaron Cruz <aaron@aaroncruz.com>
2013-01-22 21:39:40 +0100

    Initial commit

---
+++ app/assets/javascripts/interview_blog.js.coffee
@@ -0,0 +1,9 @@
+window.InterviewBlog =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  initialize: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  InterviewBlog.initialize()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InterviewBlog::Application.config.secret_token = '70662ce9fba6806c8bf515c9e4217aeff9ba8cb81f2beeee45b5ea3b3f69e183e78f5475bf5dea9bda1fd1f7b1d57a046cbda1e9e5fbfd1c8852ae0b7500e1ff'
