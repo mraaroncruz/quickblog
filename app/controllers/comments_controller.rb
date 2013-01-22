4b0fa9719b0c62ec6f670e8c57577fab8d69b5f8
Aaron Cruz <aaron@aaroncruz.com>
2013-01-23 00:03:46 +0100

    Fixed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     comment = current_user.comments.new(params[:comment])
     comment.article_id = params[:article_id]
-    redirect_to :back
+    comment.save
+    redirect_to :back, notice: "Saved your comment"
   end
 end
