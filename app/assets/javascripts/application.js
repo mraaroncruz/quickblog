d89dbe6b929a0e262097cc41c02248225a3759d7
Aaron Cruz <aaron@aaroncruz.com>
2013-01-22 23:56:20 +0100

    Removed backbone, heroku error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,12 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
-//= require underscore
-//= require backbone
-//= require interview_blog
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
 //= require_tree .
